be23664cc7afd056717d1f10423c9204e17693f2
zdylzdyl <zdylzdyl@qq.com>
2020-02-23 21:02:36 +0800

    双击一键同步至Github

--- 双击一键同步至Github.bat
+++ 双击一键同步至Github.bat
@@ -3,4 +3,5 @@ title 双击一键同步至Github
 set Path=D:\IDE\PortableGit\cmd
 cd D:
 cd D:\GithubPages
-git add * && git commit -am "双击一键同步至Github" && git push -qv github master
+git add * && git commit -am "双击一键同步至Github" && git push -qv origin master
+pause
